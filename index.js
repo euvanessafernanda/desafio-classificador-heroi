f222ca4f5357e8c26f107498cd0fd06f9ce623f1
euvanessafernanda <euvanessafernanda@gmail.com>
2026-05-19 23:51:25 -0300

    Refactor rank calculation based on victories and defeats

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-// Desafio Classificador de Nível de Herói
+// Função para calcular o saldo
+function calcularRank(vitorias, derrotas) {
 
-let nomeHeroi = "Vanessa";
-let xp = 7500;
-let nivel = "";
+    let saldo = vitorias - derrotas;
+    let nivel = "";
 
-if (xp < 1000) {
-  nivel = "Ferro";
-} else if (xp >= 1001 && xp <= 2000) {
-  nivel = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
-  nivel = "Prata";
-} else if (xp >= 5001 && xp <= 7000) {
-  nivel = "Ouro";
-} else if (xp >= 7001 && xp <= 8000) {
-  nivel = "Platina";
-} else if (xp >= 8001 && xp <= 9000) {
-  nivel = "Ascendente";
-} else if (xp >= 9001 && xp <= 10000) {
-  nivel = "Imortal";
-} else {
-  nivel = "Radiante";
+    if (saldo <= 10) {
+        nivel = "Ferro";
+    }
+    else if (saldo >= 11 && saldo <= 20) {
+        nivel = "Bronze";
+    }
+    else if (saldo >= 21 && saldo <= 50) {
+        nivel = "Prata";
+    }
+    else if (saldo >= 51 && saldo <= 80) {
+        nivel = "Ouro";
+    }
+    else if (saldo >= 81 && saldo <= 90) {
+        nivel = "Diamante";
+    }
+    else if (saldo >= 91 && saldo <= 100) {
+        nivel = "Lendario";
+    }
+    else {
+        nivel = "Imortal";
+    }
+
+    return "O Heroi tem saldo de " + saldo + " e esta no nivel de " + nivel;
 }
 
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
+// Exemplo
+let vitorias = 100;
+let derrotas = 20;
+
+console.log(calcularRank(vitorias, derrotas));
